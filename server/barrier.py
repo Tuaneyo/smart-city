10e9a2787dec6ca913677a8f2a7b7d168f8285db
Tuaneyo <nguyen.netwerk@gmail.com>
2022-12-21 15:25:15 +0100

    regsiter car count

--- server/barrier.py
+++ server/barrier.py
@@ -38,20 +38,45 @@ def close_barrier():
 
 # BUTTON FUNC
 
+def barrier_manager(BUTTON_PIN, parking_type = 'open'):
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+  previous_button_state = GPIO.input(BUTTON_PIN)
+  button_state = GPIO.input(BUTTON_PIN)
+  if button_state != previous_button_state:
+      previous_button_state = button_state
+      if button_state == GPIO.HIGH:
+          open_barrier()
+          if parking_type == 'close':
+            db.unregister_car()
+          else:
+            db.register_car()
+
 BUTTON_PIN = 12
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 previous_button_state = GPIO.input(BUTTON_PIN)
+
+BUTTON_PIN_OUT = 25
+GPIO.setup(BUTTON_PIN_OUT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+previous_button_state2 = GPIO.input(BUTTON_PIN_OUT)
 
 try:
     while True:
         sleep(0.01)
         button_state = GPIO.input(BUTTON_PIN)
         if button_state != previous_button_state:
-            previous_button_state = button_state
-            if button_state == GPIO.HIGH:
-                open_barrier()
+          previous_button_state = button_state
+          if button_state == GPIO.HIGH:
+              open_barrier()
+              db.register_car()
 
+        button_state = GPIO.input(BUTTON_PIN_OUT)
+        if button_state != previous_button_state2:
+          previous_button_state2 = button_state
+          if button_state == GPIO.HIGH:
+              open_barrier()
+              db.unregister_car()
 except KeyboardInterrupt:
     GPIO.cleanup()
 
--- server/db.py
+++ server/db.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import uuid
 
 cred = credentials.Certificate('/home/pi/smart-city/smart-city/server/firebaseServiceKey.json')
 
@@ -28,4 +29,26 @@ def get_free_space():
 
     return spaces_text.rstrip(', ')
 
-get_free_space()
+
+def register_car():
+    cars_ref = db.collection(u'parking').document('cars').get()
+    cars_count = cars_ref.to_dict()['count']
+    print(cars_count)
+    data = {
+        u'count': cars_count + 1
+    }
+    db.collection(u'parking').document(u'cars').set(data, merge=True)
+
+
+def unregister_car():
+    cars_ref = db.collection(u'parking').document('cars').get()
+    cars_count = cars_ref.to_dict()['count']
+    print(cars_count)
+    data = {
+        u'count': cars_count - 1
+    }
+    db.collection(u'parking').document(u'cars').set(data, merge=True)
+
+
+
+register_car()
